f8b106991b75709372653bd53461ca6ceb76390c
CLLiebe <54807651+CLLiebe@users.noreply.github.com>
2021-02-18 18:07:25 -0700

    Update Lab05_TallyforKids.py
    changed variable name

--- Lab05_TallyforKids.py
+++ Lab05_TallyforKids.py
@@ -3,7 +3,7 @@
 
 print("Enter values to add. Enter quit when done.")
 num = 0
-count = 0
+count1 = 0
 total = 0
 num = input("NUMBER> ")
 
@@ -12,7 +12,7 @@ while num != "quit":
         print("Input invalid. If you want to stop enter: quit")
         num = input("NUMBER> ")
     else:
-        count += 1
+        count1 += 1
         num = int(num)
         total += num
         num = input("NUMBER> ")
@@ -20,7 +20,7 @@ while num != "quit":
 
     
 print(f"The addition of the {count} numbers entered is:")
-print("OUTPUT", count, "numbers")
+print("OUTPUT", count1, "numbers")
 print("OUTPUT", total, "total")
 print("Goodbye!")
         
