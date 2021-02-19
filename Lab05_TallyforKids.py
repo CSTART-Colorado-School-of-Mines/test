250d2c4a96a269afc919260d4b63a99963f60fb6
CLLiebe <54807651+CLLiebe@users.noreply.github.com>
2021-02-18 17:56:34 -0700

    Add files via upload
    while loop demo

---
+++ Lab05_TallyforKids.py
@@ -0,0 +1,31 @@
+#Lab05B TallyForKids
+
+
+print("Enter values to add. Enter quit when done.")
+num = 0
+count = 0
+total = 0
+num = input("NUMBER> ")
+
+while num != "quit":
+    if num == "Quit" or num == "QUIT":
+        print("Input invalid. If you want to stop enter: quit")
+        num = input("NUMBER> ")
+    else:
+        count += 1
+        num = int(num)
+        total += num
+        num = input("NUMBER> ")
+    
+
+    
+print(f"The addition of the {count} numbers entered is:")
+print("OUTPUT", count, "numbers")
+print("OUTPUT", total, "total")
+        
+
+ 
+    
+
+
+    
